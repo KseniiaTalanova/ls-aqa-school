f3ccfdd76854fe385122d31bbe22bcd8ef05410f
voronin-pavel <pavel.voronin@ligastavok.ru>
2024-09-30 14:12:58 +0300

    Добавил гитигнор

--- ls-java-core/src/main/java/ru/ls/qa/school/core/OddTask.java
+++ ls-java-core/src/main/java/ru/ls/qa/school/core/OddTask.java
@@ -2,13 +2,7 @@ package ru.ls.qa.school.core;
 
 public class OddTask {
     public static boolean isOdd (int number){
-        if (number%2 == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
-
+        return number % 2 == 0;
     }
 
     public static void main(String[] args) {
